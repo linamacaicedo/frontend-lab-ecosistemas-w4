be13fb86425d3e46ec2ec3ea1c224b9bc09e2fa5
Lina Maria Caicedo <linamacaicedo@gmail.com>
2026-04-24 19:11:34 -0500

    fixin front bugs for vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://backend-lab-ecosistemas-w4.vercel.app/api"
 });
 
 export default API;
